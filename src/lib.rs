d5bb7eefa1656ca077a3cea833b7b327c19aa8ea
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-06 08:56:30 +1000

    Add cfg

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
+#[cfg(jacobi)]
 mod jacobi;
+
+#[cfg(factorization)]
 mod pollard_fact;
+
+#[cfg(logarithm)]
 mod pollard_log;
+
+#[cfg(powmod)]
 mod powmod;
+
+#[cfg(primality)]
 mod primality;
+
+#[cfg(primegen)]
 mod prime_gen;
+
+#[cfg(sqrt)]
 mod sqrt;
